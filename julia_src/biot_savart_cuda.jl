aec6ceada07d452128c0aa7022c512f950cd0c76
kbcollins <kbcollins@users.noreply.github.com>
2024-02-14 12:39:40 -0800

    Committing current progress before branching.

--- julia_src/biot_savart_cuda.jl
+++ julia_src/biot_savart_cuda.jl
@@ -112,6 +112,74 @@ function bs_uniform_trapezoidal_rule(numsteps, fp, vorpps, corerads, circs, segi
     return sol .* (stepsize / (4 * pi))
 end
 
+# User API for the Biot-Savart kernel
+function biot_savart(
+    fieldpoints,
+    vortexpathpoints,
+    vortexcoreradii,
+    vortexcirculations)
+
+    rtnvel = zeros(Float32, 3, size(fieldpoints, 2))
+    # Set the number of threads per block
+    numthreads = 256
+    numblocks = ceil(Int, numfp / numthreads)
+
+    # THIS IS TEMPORARY UNTIL I FIGURE OUT
+    # HOW TO PASS LARGE ARRAYS TO THE KERNEL
+    batchsize = 10_000
+    numbatches = ceil(Int, size(fieldpoints, 2) / batchsize)
+    # @show numbatches  # DEBUG
+
+    # Initialize the batch variables
+    rtnvelbatch = CuArray{Float32}(undef, 3, batchsize)
+    fldpntbatch = CuArray{Float32}(undef, 3, batchsize)
+    vtxpnts = CuArray{Float32}(vortexpathpoints)
+    vtxcors = CuArray{Float32}(vortexcoreradii)
+    vtxcirs = CuArray{Float32}(vortexcirculations)
+
+    # Loop through the batches of field points
+    batchindex = 1
+    while batchindex < numbatches
+        # Get a batch of field points
+        btchindxstrt = 1 + ((batchindex - 1) * batchsize)
+        # @show btchindxstrt  # DEBUG
+        btchindxend = batchindex * batchsize
+        # @show btchindxend  # DEBUG
+        # fldpntbatch .= fieldpoints[:, btchindxstrt:btchindxend]
+
+        # Call the kernel
+        # @device_code_warntype 
+        # @device_code_llvm
+        # @device_code_lowered
+        @cuda blocks=numblocks threads=numthreads weighted_biot_savart_kernel(
+            rtnvelbatch,
+            fldpntbatch,
+            vtxpnts,
+            vtxcors,
+            vtxcirs)
+
+        # Copy the batch results to the return array
+        rtnvel[:, btchindxstrt:btchindxend] .= Array(rtnvelbatch)
+
+        # Advance the batch index
+        batchindex += batchsize
+    end
+
+    lastbatchstart = 1 + ((batchindex - 1) * batchsize)
+    fldpntbatchlast = CuArray{Float32}(fieldpoints[:, lastbatchstart:end])
+    rtnvelbatchlast = CuArray{Float32}(undef, 3, size(fldpntbatchlast, 2))
+    # Call the kernel
+    @cuda blocks=numblocks threads=numthreads weighted_biot_savart_kernel(
+    rtnvelbatchlast,
+    fldpntbatchlast,
+    vtxpnts,
+    vtxcors,
+    vtxcirs)
+    rtnvel[:, lastbatchstart:end] .= Array(rtnvelbatchlast)
+
+    return rtnvel
+end
+
 
 ################ Biot-Savart kernel ################
 
@@ -165,7 +233,7 @@ LinearAlgebra functions on the GPU
 =#
 function weighted_biot_savart_kernel(
     returnvelocities,
-    fieldpointsbatch,
+    fieldpoints,
     vortexpathpoints,
     vortexcoreradii,
     vortexcirculations)
@@ -176,41 +244,44 @@ function weighted_biot_savart_kernel(
     # Compute the global index of the thread
     idx = (blockIdx().x - Int32(1)) * blockDim().x + threadIdx().x
 
-    # Get this thread's field point from the batch
-    @inbounds fieldpoint = SVector{3, Float32}(
-        fieldpointsbatch[1, idx],
-        fieldpointsbatch[2, idx],
-        fieldpointsbatch[3, idx])
+    # Check if the thread index is out of bounds
+    if idx <= Int32(size(fieldpoints, 2))
+        # Get this thread's field point from the batch
+        @inbounds fldpnt = SVector{3, Float32}(
+            fieldpoints[1, idx],
+            fieldpoints[2, idx],
+            fieldpoints[3, idx])
 
-    # Initialize the velocity at the field point
-    vel = MVector{3, Float32}(0, 0, 0)
+        # Initialize the velocity at the field point
+        vel = MVector{3, Float32}(0, 0, 0)
 
-    # Step through each vortex path segment.
-    # We are using a while loop because the CUDA.jl
-    # docs says this is more efficient than using a
-    # for loop with a step interval.
-    segindex = UInt32(1)
-    while segindex <= numvpsegs
+        # Step through each vortex path segment.
+        # We are using a while loop because the CUDA.jl
+        # docs says this is more efficient than using a
+        # for loop with a step interval.
+        segindex = UInt32(1)
+        while segindex <= numvpsegs
 
-        vel .+= bs_uniform_trapezoidal_rule(
-            UInt32(1000),  # Number of integrations steps
-            fieldpoint,
-            vortexpathpoints,
-            vortexcoreradii,
-            vortexcirculations,
-            segindex,
-            Float32(0.5))
+            vel .+= bs_uniform_trapezoidal_rule(
+                UInt32(1000),  # Number of integrations steps
+                fldpnt,
+                vortexpathpoints,
+                vortexcoreradii,
+                vortexcirculations,
+                segindex,
+                Float32(0.5))
 
-        segindex += UInt32(1)  # Advance the loop counter
+            segindex += UInt32(1)  # Advance the loop counter
+        end
+
+        returnvelocities[:, idx] .= vel
     end
 
-    returnvelocities[:, idx] .= vel
     return nothing
 end
 
 
-
-#============ Straight Vortex Test Case ============#
+################ Straight Vortex Test Case ################
 #=
 **Straight Vortex Test Case**
 This is the start of a unit test of the
@@ -226,79 +297,47 @@ using Plots
 numvpsegs = 2
 vp = zeros(Float32, 3, numvpsegs + 1)
 vp[1, :] .= range(-1000, stop=1000, length=numvpsegs + 1)
-vp = CuArray{Float32}(vp)
-vc = CUDA.ones(Float32, numvpsegs + 1)
+# vp = CuArray{Float32}(vp)
+vc = ones(Float32, numvpsegs + 1)
+# vc = CUDA.ones(Float32, numvpsegs + 1)
 
 # Define the circulation at each path point
-cir = CUDA.ones(Float32, numvpsegs + 1)
+cir = ones(Float32, numvpsegs + 1)
+# cir = CUDA.ones(Float32, numvpsegs + 1)
 
 # Define the field points
 numfp = 10_000
 y = zeros(Float32, numfp)
 y .= range(0, 20, length=numfp)  # end point is included in range
-y[1] = 1e-3  # avoid divide by zero
+y[1] = 1.0  # 1e-3  # avoid divide by zero
 fp = zeros(Float32, 3, numfp)
 fp[2, :] = y
-fp = CuArray{Float32}(fp)
+# fp = CuArray{Float32}(fp)
 
-# Initialize velocity array
-vel_num = zeros(3, numfp)
-# println("size(vel_num) = ", size(vel_num))  # DEBUG
 
-numblocks = 4
-numthreads = 1024
-fpbatchsize = numblocks * numthreads
-numbatches = ceil(Int, size(fp, 2) / fpbatchsize)
-fpbatchindex = 1
-for fpbatchindex in 1:(numbatches-1)
-    println("On batch ", fpbatchindex, " of ", numbatches, ".")
+# # Create the return velocities array
+# # CuArray is mutable!!!
+# rntvel = CuArray{Float32}(undef, 3, numfp)
+# # Initialize velocity array
+# vel_num = zeros(3, numfp)
+# # println("size(vel_num) = ", size(vel_num))  # DEBUG
 
-    # Create the return velocities array
-    # CuArray is mutable!!!
-    rtnv = CuArray{Float32}(undef, 3, fpbatchsize)
-    # println("size(rtnv) = ", size(rtnv))  # DEBUG
-    # display(rtnv)
+# numthreads = 256
+# numblocks = ceil(Int, numfp / numthreads)
 
-    # Get a batch of: field points
-    spanstart = 1 + ((fpbatchindex - 1) * fpbatchsize)
-    # @show spanstart  # DEBUG
-    spanend = fpbatchindex * fpbatchsize
-    # @show spanend  # DEBUG
-    fpbatch = fp[:, spanstart:spanend]
+# # Call the kernel
+# # @device_code_warntype 
+# # @device_code_llvm
+# # @device_code_lowered
+# @cuda blocks=numblocks threads=numthreads weighted_biot_savart_kernel(
+#     rntvel,
+#     fp,
+#     vp,
+#     vc,
+#     cir)
 
-    println("\tSending to CUDA kernel...")
-    # Call the kernel
-    # @device_code_warntype 
-    # @device_code_llvm
-    # @device_code_lowered
-    @cuda blocks=numblocks threads=numthreads weighted_biot_savart_kernel(
-        rtnv,
-        fpbatch,
-        vp,
-        vc,
-        cir)
-
-    # print("rtnv (after):")
-    # display(rtnv)
-
-    println("\tCopying to host...")
-    vel_num[:, spanstart:spanend] .= Array(rtnv)
-end
-
-# Do last batch separately (whatever is left over)
-spnstrt = 1 + ((numbatches - 1) * fpbatchsize)
-fpbtch = fp[:, spnstrt:end]
-rv = CuArray{Float32}(undef, 3, size(fpbtch, 2))
-nblks = 2
-nthrds = ceil(Int, size(fpbtch, 2) / nblks)
-@cuda blocks=nblks threads=nthrds weighted_biot_savart_kernel(
-    rv,
-    fpbtch,
-    vp,
-    vc,
-    cir)
-
-vel_num[:, spnstrt:end] .= Array(rv)
+# vel_num .= Array(rntvel)
+vel_num = biot_savart(fp, vp, vc, cir)
 
 vel_true = 1 ./ (2 .* pi .* y)
 # println("vel_true = ", vel_true)
