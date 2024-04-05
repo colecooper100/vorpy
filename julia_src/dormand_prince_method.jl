811f53770beb1ec5c141777fcdd142c68e9b8ff8
kbcollins <kbcollins@users.noreply.github.com>
2024-04-05 13:01:47 -0700

    Improved logic in Dormand-Prince solver.

--- julia_src/dormand_prince_method.jl
+++ julia_src/dormand_prince_method.jl
@@ -77,53 +77,46 @@ end
 
 function dormand_prince_adaptive_step(fn, tspan, x0, min_step_size, max_step_size, loc_err_tol)
     # Initial stepsize
-    # initial_val_fn = fn(tspan[1], x0)
-    # stepsize = 1f-1 / sqrt(sum(initial_val_fn) .^ 2 / length(initial_val_fn))
-    stepsize = 1f0  # DEBUG
-    # print("Initial stepsize: ", stepsize)  # DEBUG
+    initial_val_fn = fn(tspan[1], x0)
+    stepsize = 1f-1 / sqrt(sum(initial_val_fn) .^ 2 / length(initial_val_fn))
+    println("Initial stepsize: ", stepsize)  # DEBUG
 
     tsteps = [tspan[1]]
-    x = [x0]
+    solvec = [x0]
     steperr = []
     while tsteps[end] <= tspan[2]
-        sol, local_err = dormand_prince_step(fn, tsteps[end], x[end], stepsize)
+        sol, local_err = dormand_prince_step(fn, tsteps[end], solvec[end], stepsize)
 
-        #===========================================
-        Here, we just decrease the stepsize by a
-        factor of 2 and use a simple method for
-        guessing the \Delta t_{n+1} step size.
-        However, more sophisticated method for
-        selecting a stepsize exist. This will be
-        implemented later.
-        ===========================================#
-        if local_err > loc_err_tol && min_step_size <= stepsize <= max_step_size
-            # println("The local error is greater than the specified error tolerance of ", loc_err_tol, ". Decreasing the step size by a factor of 2.")  # DEBUG
-            stepsize /= 2
-        elseif stepsize > max_step_size 
-            println("The step size has been set to ", stepsize, " which is larger than the specified maximum step size of ", max_step_size, ". Try increasing the maximum step size. Continuing with the maximum step size.")
+        if stepsize > max_step_size
+            # Step too big
+            println("Time step $(length(tsteps)): The new step size is greater than the maximum step size. Continuing with the maximum step size.")
             stepsize = max_step_size
-        elseif local_err > loc_err_tol && stepsize < min_step_size
-            println("The step size has been set to ", stepsize, " but the local error is ", local_err, " which is larger than the specified error tolerance of ", loc_err_tol, ". Continuing with the minimum step size.")
+        elseif stepsize < min_step_size
+            # Step too small
+            println("Time step $(length(tsteps)): The size is less than the minimum step size but the local error is greater than the specified tolerance. Continuing with the minimum step size.")
             stepsize = min_step_size
-        else
-            push!(tsteps, tsteps[end] + stepsize)
-            push!(x, sol)
-            push!(steperr, local_err)
-            # Set the stepsize for the next step
-            ssize = comp_step_size(stepsize, local_err, loc_err_tol, 4, 0.9)  # DEBUG
-            println("Step size: ", stepsize, " Local error: ", local_err, " Next stepsize: ", ssize)  # DEBUG
-            # stepsize = 1f0  # DEBUG
-            stepsize = ssize
-            # stepsize = 1f-1 / sqrt(sum(fn(tsteps[end], sol) .^ 2) / length(initial_val_fn))
+        elseif isnan(local_err) || isinf(local_err)
+            # Local error is NaN or Inf
+            println("Time step $(length(tsteps)): The local error is $(local_err). Continuing with the minimum step size.")
+            stepsize = min_step_size
+        else # min_step_size <= stepsize <= max_step_size
+            if local_err < loc_err_tol
+                # Accept step
+                push!(tsteps, tsteps[end] + stepsize)
+                push!(solvec, sol)
+                push!(steperr, local_err)
+            end
+            # Get new step size using the local error
+            stepsize = new_step_size(stepsize, local_err, loc_err_tol, 4, 0.9)
         end
     end
 
-    return x, tsteps, steperr
+    return solvec, tsteps, steperr
     # return stack(x), tsteps, steperr
 end
 
-function comp_step_size(stepsize, local_err, loc_err_tol, global_err_order, safety_factor)
-    return safety_factor * ((loc_err_tol / local_err) * stepsize ^ (global_err_order + 1)) ^ (1 / (global_err_order + 1))
+function new_step_size(stepsize, local_err, loc_err_tol, global_err_order, safety_factor)
+    return safety_factor * stepsize * (loc_err_tol / local_err) ^ (1 / (global_err_order + 1))
 end
 
 
@@ -140,6 +133,9 @@ function scalar_time_independent_sol(t, x)
     return 2t
 end
 
+scalar_time_independent_x0 = 0f0
+
+
 # Problem: x' = 2t
 # Solution: x(t) = t^2
 function scalar_time_depend(t, x)
@@ -150,6 +146,9 @@ function scalar_time_depend_sol(t, x)
     return t^2
 end
 
+scalar_time_depend_x0 = 0f0
+
+
 # Solution: x(t) = [sin(t), cos(t)]
 function vector_time_independent(t, x)
     return [x[2], -x[1]]
@@ -159,23 +158,27 @@ function vector_time_independent_sol(t, x)
     return [sin(t), cos(t)]
 end
 
+vector_time_independent_x0 = [0f0, 1f0]
 
-func = vector_time_independent
-solfn = vector_time_independent_sol
+
+func = scalar_time_depend
+solfn = scalar_time_depend_sol
 t0 = Float32(0)
 tf = Float32(10)
 tspan = [t0, tf]
-x0 = [0f0, 1f0]
+x0 = scalar_time_depend_x0
 min_step_size = 1f-6
-max_step_size = 5f0
+max_step_size = Inf32
+err_tol = 1f-3
 
 println("Initial time: ", t0)
 println("Final time: ", tf)
 println("Initial condition: ", x0)
 println("Minimum step size: ", min_step_size)
 println("Maximum step size: ", max_step_size)
+println("Error tolerance: ", err_tol)
 
-xvec, timesteps, steperror = dormand_prince_adaptive_step(func, tspan, x0, min_step_size, max_step_size, 1f-3)
+xvec, timesteps, steperror = dormand_prince_adaptive_step(func, tspan, x0, min_step_size, max_step_size, err_tol)
 xvec = stack(xvec)
 
 xvecana = [solfn(timesteps[1], x0)]
@@ -184,6 +187,7 @@ for i in 2:length(timesteps)
 end
 xvecana = stack(xvecana)
 
-plot(timesteps, [xvecana[1, :], xvecana[2, :]], label="Analytical", markershape=:x)
-plot!(timesteps, [xvec[1, :], xvec[2, :]], label="Numerical", markershape=:none)
-# # plot!(timesteps, [xvec2[1, :], xvec2[2, :]], label="Numerical2", markershape=:none)
+plot(timesteps, xvecana, label="Analytical", markershape=:x)
+plot!(timesteps, xvec, label="Numerical", markershape=:none)
+# plot(timesteps, [xvecana[1, :], xvecana[2, :]], label="Analytical", markershape=:x)
+# plot!(timesteps, [xvec[1, :], xvec[2, :]], label="Numerical", markershape=:none)
