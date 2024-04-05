e018fbf6c2cf0cb64fc4262bc22cb660258efac4
kbcollins <kbcollins@users.noreply.github.com>
2024-04-05 09:48:10 -0700

    Adaptive Dormand-Prince method works as expected and providing good results!

--- julia_src/dormand_prince_method.jl
+++ julia_src/dormand_prince_method.jl
@@ -1,4 +1,3 @@
-
 #==========================================
 Use the Dormand-Prince method to
 solve:
@@ -16,79 +15,175 @@ Start with a constant stepsize then
 make an adaptive method
 ==========================================#
 
-function constant_v(t, x)
-    return 2
+
+# \vec x' = f(t, \vec x)
+function dormand_prince_step(fn, t0, x0, stepsize)
+    c2 = Float32(1/5)
+    a21 = Float32(1/5)
+    c3 = Float32(3/10)
+    a31 = Float32(3/40)
+    a32 = Float32(9/40)
+    c4 = Float32(4/5)
+    a41 = Float32(44/45)
+    a42 = Float32(-56/15)
+    a43 = Float32(32/9)
+    c5 = Float32(8/9)
+    a51 = Float32(19372/6561)
+    a52 = Float32(-25360/2187)
+    a53 = Float32(64448/6561)
+    a54 = Float32(-212/729)
+    c6 = Float32(1)
+    a61 = Float32(9017/3168)
+    a62 = Float32(-355/33)
+    a63 = Float32(46732/5247)
+    a64 = Float32(49/176)
+    a65 = Float32(-5103/18656)
+    c7 = Float32(1)
+    a71 = Float32(35/84)
+    a72 = Float32(0)
+    a73 = Float32(500/1113)
+    a74 = Float32(125/192)
+    a75 = Float32(-2187/6784)
+    a76 = Float32(11/84)
+    b1 = Float32(35/384)
+    b2 = Float32(0)
+    b3 = Float32(500/1113)
+    b4 = Float32(125/192)
+    b5 = Float32(-2187/6784)
+    b6 = Float32(11/84)
+    bhat1 = Float32(5179/57600)
+    bhat2 = Float32(0)
+    bhat3 = Float32(7571/16695)
+    bhat4 = Float32(393/640)
+    bhat5 = Float32(-92097/339200)
+    bhat6 = Float32(187/2100)
+    bhat7 = Float32(1/40)
+
+    k1 = fn(t0, x0)
+    k2 = fn(t0 + c2 * stepsize, x0 .+ stepsize * (a21 .* k1))
+    k3 = fn(t0 + c3 * stepsize, x0 .+ stepsize * (a31 .* k1 .+ a32 .* k2))
+    k4 = fn(t0 + c4 * stepsize, x0 .+ stepsize * (a41 .* k1 .+ a42 .* k2 .+ a43 .* k3))
+    k5 = fn(t0 + c5 * stepsize, x0 .+ stepsize * (a51 .* k1 .+ a52 .* k2 .+ a53 .* k3 .+ a54 .* k4))
+    k6 = fn(t0 + c6 * stepsize, x0 .+ stepsize * (a61 .* k1 .+ a62 .* k2 .+ a63 .* k3 .+ a64 .* k4 .+ a65 .* k5))
+    k7 = fn(t0 + c7 * stepsize, x0 .+ stepsize * (a71 .* k1 .+ a72 .* k2 .+ a73 .* k3 .+ a74 .* k4 .+ a75 .* k5 .+ a76 .* k6))
+
+    sol4 = x0 .+ stepsize * (b1 .* k1 .+ b2 .* k2 .+ b3 .* k3 .+ b4 .* k4 .+ b5 .* k5 .+ b6 .* k6)
+    sol5 = x0 .+ stepsize * (bhat1 .* k1 .+ bhat2 .* k2 .+ bhat3 .* k3 .+ bhat4 .* k4 .+ bhat5 .* k5 .+ bhat6 .* k6 .+ bhat7 .* k7)
+
+    local_err = sqrt(sum((sol5 - sol4) .^ 2) / length(sol4))  # RMS error
+
+    return sol4, local_err
 end
 
-function time_depend_v(t, x)
-    return 2*t
+function dormand_prince_adaptive_step(fn, tspan, x0, min_step_size, max_step_size, loc_err_tol)
+    # Initial stepsize
+    # initial_val_fn = fn(tspan[1], x0)
+    # stepsize = 1f-1 / sqrt(sum(initial_val_fn) .^ 2 / length(initial_val_fn))
+    stepsize = 1f0  # DEBUG
+    # print("Initial stepsize: ", stepsize)  # DEBUG
+
+    tsteps = [tspan[1]]
+    x = [x0]
+    steperr = []
+    while tsteps[end] <= tspan[2]
+        sol, local_err = dormand_prince_step(fn, tsteps[end], x[end], stepsize)
+
+        #===========================================
+        Here, we just decrease the stepsize by a
+        factor of 2 and use a simple method for
+        guessing the \Delta t_{n+1} step size.
+        However, more sophisticated method for
+        selecting a stepsize exist. This will be
+        implemented later.
+        ===========================================#
+        if local_err > loc_err_tol && min_step_size <= stepsize <= max_step_size
+            # println("The local error is greater than the specified error tolerance of ", loc_err_tol, ". Decreasing the step size by a factor of 2.")  # DEBUG
+            stepsize /= 2
+        elseif stepsize > max_step_size 
+            println("The step size has been set to ", stepsize, " which is larger than the specified maximum step size of ", max_step_size, ". Try increasing the maximum step size. Continuing with the maximum step size.")
+            stepsize = max_step_size
+        elseif local_err > loc_err_tol && stepsize < min_step_size
+            println("The step size has been set to ", stepsize, " but the local error is ", local_err, " which is larger than the specified error tolerance of ", loc_err_tol, ". Continuing with the minimum step size.")
+            stepsize = min_step_size
+        else
+            push!(tsteps, tsteps[end] + stepsize)
+            push!(x, sol)
+            push!(steperr, local_err)
+            # Set the stepsize for the next step
+            ssize = comp_step_size(stepsize, local_err, loc_err_tol, 4, 0.9)  # DEBUG
+            println("Step size: ", stepsize, " Local error: ", local_err, " Next stepsize: ", ssize)  # DEBUG
+            # stepsize = 1f0  # DEBUG
+            stepsize = ssize
+            # stepsize = 1f-1 / sqrt(sum(fn(tsteps[end], sol) .^ 2) / length(initial_val_fn))
+        end
+    end
+
+    return x, tsteps, steperr
+    # return stack(x), tsteps, steperr
 end
 
-
-
-vfn(t, x0) = time_depend_v(t, x0)
-
-
-function dormand_prince_step(stepsize, t, x)
-    k1 = vfn(t, x)
-    k2 = vfn(t + stepsize/5, x + stepsize/5*k1)
-    k3 = vfn(t + 3*stepsize/10, x + 3*stepsize/40*k1 + 9*stepsize/40*k2)
-    k4 = vfn(t + 4*stepsize/5, x + 44*stepsize/45*k1 - 56*stepsize/15*k2 + 32*stepsize/9*k3)
-    k5 = vfn(t + 8*stepsize/9, x + 19372*stepsize/6561*k1 - 25360*stepsize/2187*k2 + 64448*stepsize/6561*k3 - 212*stepsize/729*k4)
-    k6 = vfn(t + stepsize, x + 9017*stepsize/3168*k1 - 355*stepsize/33*k2 + 46732*stepsize/5247*k3 + 49*stepsize/176*k4 - 5103*stepsize/18656*k5)
-    k7 = vfn(t + stepsize, x + stepsize*(35/384*k1 + 500/1113*k3 + 125/192*k4 - 2187/6784*k5 + 11/84*k6))
-
-    sol4 = x + stepsize*(5179/57600*k1 + 7571/16695*k3 + 393/640*k4 - 92097/339200*k5 + 187*stepsize/2100*k6 + 1/40*k7)
-    sol5 = x + stepsize*(35/384*k1 + 500/1113*k3 + 125/192*k4 - 2187/6784*k5 + 11/84*k6)
-    err = abs(sol5 - sol4)
-    return sol5, err
+function comp_step_size(stepsize, local_err, loc_err_tol, global_err_order, safety_factor)
+    return safety_factor * ((loc_err_tol / local_err) * stepsize ^ (global_err_order + 1)) ^ (1 / (global_err_order + 1))
 end
-
-# function adaptive_stepsize_dormand_prince_method(t, x, tol)
-#     stepsize = 1e-3
-#     sol5, err = dormand_prince_step(stepsize, t, x)
-#     while norm(err) > tol
-#         stepsize /= 2
-#         sol5, err = dormand_prince_step(stepsize, t, x)
-#     end
-#     return sol5
-# end
 
 
 ################## Test ##################
 using Plots
 
-X0 = 3
-T0 = 3
-TF = 10
-# stepsize = 1
-# xplt = plot()
-# toterrplt = plot()
-# locerrplt = plot()
-for stepsize in [1e-2, 1e-1, 1, 2, 4]
-    tvec = T0:stepsize:TF
-
-    # ana_sol_const_v = X0 .+ 2*(tvec .- T0)  # constant_v
-    ana_sol_const_v = X0 - T0^2 .+ tvec.^2  # time_depend_v
-    num_sol_const_v = zeros(length(tvec))
-    loc_err = zeros(length(tvec))
-    num_sol_const_v[1] = X0
-    for i in 2:length(tvec)
-        num_sol_const_v[i], loc_err[i] = dormand_prince_step(stepsize, tvec[i-1], num_sol_const_v[i-1])
-    end
-
-    xplt = plot()
-    toterrplt = plot()
-    locerrplt = plot()
-
-    plot!(xplt, tvec, ana_sol_const_v, label="Analytical", markershape=:x)
-    plot!(xplt, tvec, num_sol_const_v, label="Numerical", markershape=:none)
-
-
-    plot!(toterrplt, tvec, abs.(ana_sol_const_v - num_sol_const_v), label="Error")
-    plot!(locerrplt, tvec, loc_err, label="Local Error")
-
-    display(plot(xplt, toterrplt, locerrplt, layout=(3, 1), legend=true))
+# Problem: x' = 2
+# Solution: x(t) = 2t
+function scalar_time_independent(t, x)
+    return 2
 end
 
-# display(plot(xplt, toterrplt, locerrplt, layout=(3, 1), legend=true))
+function scalar_time_independent_sol(t, x)
+    return 2t
+end
+
+# Problem: x' = 2t
+# Solution: x(t) = t^2
+function scalar_time_depend(t, x)
+    return 2*t
+end
+
+function scalar_time_depend_sol(t, x)
+    return t^2
+end
+
+# Solution: x(t) = [sin(t), cos(t)]
+function vector_time_independent(t, x)
+    return [x[2], -x[1]]
+end
+
+function vector_time_independent_sol(t, x)
+    return [sin(t), cos(t)]
+end
+
+
+func = vector_time_independent
+solfn = vector_time_independent_sol
+t0 = Float32(0)
+tf = Float32(10)
+tspan = [t0, tf]
+x0 = [0f0, 1f0]
+min_step_size = 1f-6
+max_step_size = 5f0
+
+println("Initial time: ", t0)
+println("Final time: ", tf)
+println("Initial condition: ", x0)
+println("Minimum step size: ", min_step_size)
+println("Maximum step size: ", max_step_size)
+
+xvec, timesteps, steperror = dormand_prince_adaptive_step(func, tspan, x0, min_step_size, max_step_size, 1f-3)
+xvec = stack(xvec)
+
+xvecana = [solfn(timesteps[1], x0)]
+for i in 2:length(timesteps)
+    append!(xvecana, [solfn(timesteps[i], xvecana[end])])
+end
+xvecana = stack(xvecana)
+
+plot(timesteps, [xvecana[1, :], xvecana[2, :]], label="Analytical", markershape=:x)
+plot!(timesteps, [xvec[1, :], xvec[2, :]], label="Numerical", markershape=:none)
+# # plot!(timesteps, [xvec2[1, :], xvec2[2, :]], label="Numerical2", markershape=:none)
